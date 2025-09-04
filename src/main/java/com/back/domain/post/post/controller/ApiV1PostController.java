a08de30c1a1cd8d2596548f67cd3f8a13c05dfb7
SWWWin <bgpd1258@naver.com>
2025-09-04 10:47:41 +0900

    feat: ForPostRsData로 임시 해결

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -5,6 +5,8 @@ import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
+import com.back.global.rsData.ForPostRsData;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -42,11 +44,11 @@ public class ApiV1PostController {
 
     @Transactional
     @GetMapping("/{id}/delete")
-    public PostDto delete(@PathVariable Long id) {
+    public ForPostRsData delete(@PathVariable Long id) {
         Post post = postService.findById(id);
 
         postService.delete(post);
 
-        return new PostDto(post);
+        return new  ForPostRsData("200-1", "%d 번 게시글이 삭제되었습니다.".formatted(id), new PostDto(post));
     }
 }
