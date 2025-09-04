88f2e6ed9264dd0cae8551ed86f924318bca09bf
SWWWin <bgpd1258@naver.com>
2025-09-04 10:25:47 +0900

    feat: 표준화 - rsData 분리

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -4,6 +4,7 @@ import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,7 +47,7 @@ public class ApiV1PostCommentController {
 
     @Transactional
     @GetMapping("/{commentId}/delete")
-    public Map<String, Object> delete(
+    public RsData delete(
             @PathVariable long postId,
             @PathVariable Long commentId
     ) {
@@ -56,9 +57,6 @@ public class ApiV1PostCommentController {
 
         postService.deleteComment(post, postComment);
 
-        Map<String, Object> rsData = new LinkedHashMap<>();
-        rsData.put("resultCode", 200);
-        rsData.put("msg", "%d 댓글이 삭제되었습니다.");
-        return rsData;
+        return new RsData("200-1", "%d 댓글이 삭제되었습니다.".formatted(commentId));
     }
 }
