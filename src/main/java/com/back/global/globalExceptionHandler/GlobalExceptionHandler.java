455c7b0ef0343a7ca90ff9a1bc83a98009576354
SWWWin <bgpd1258@naver.com>
2025-09-09 12:39:30 +0900

    feat: TDD - 단건 404-2

--- src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -3,20 +3,25 @@ package com.back.global.globalExceptionHandler;
 import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
 
-@RestControllerAdvice// 전처리의 개념
+import static ch.qos.logback.core.util.AggregationType.NOT_FOUND;
+
+@ControllerAdvice// 전처리의 개념
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchElementException.class)
-    public RsData<Void> handle() {
-        return new RsData<>(
-                "404-1",
-                "해당 데이터가 존재하지 않습니다."
+    public ResponseEntity<RsData<Object>> handle() {
+        return new ResponseEntity<>(
+                new RsData<>(
+                        "404-1",
+                        "해당 데이터가 존재하지 않습니다."
+                ),
+                HttpStatus.NOT_FOUND
         );
     }
 }
