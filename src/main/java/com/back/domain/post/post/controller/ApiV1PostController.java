f58f399b98e490e94a77321d551a54628700030f
SWWWin <bgpd1258@naver.com>
2025-09-18 14:46:07 +0900

    feat: 헤더 검증 코드 변경

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -7,6 +7,7 @@ import com.back.domain.post.post.dto.PostModifyReqBody;
 import com.back.domain.post.post.dto.PostWriteReqBody;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import com.back.global.Rq.Rq;
 import com.back.global.exception.ServiceException;
 import com.back.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
@@ -29,11 +30,15 @@ import java.util.List;
 public class ApiV1PostController {
     private final PostService postService;
     private final MemberService memberService;
+    private final Rq rq;
 
     @Transactional(readOnly = true)
     @GetMapping(produces = "application/json")
     @Operation(summary = "다건")
     public List<PostDto> getItems() {
+        System.out.println("memberService: " + memberService);
+        System.out.println("rq: " + rq);
+        System.out.println("rq.getActor: " + rq.getActor());
         return postService.getList()
                 .stream()
                 .map(PostDto::new)
@@ -71,10 +76,9 @@ public class ApiV1PostController {
             @Valid @RequestBody PostWriteReqBody reqBody,
             @NotBlank @Size(min = 2, max = 50) @RequestHeader("Authorization") String authorization
     ) {
-        String apiKey = authorization.replace("Bearer ", "");
+        System.out.println("rq.getActor: " + rq.getActor());
 
-        Member author = memberService.findByApiKey(apiKey)
-                .orElseThrow(() -> new ServiceException("401-1", "존재하지 않는 회원입니다."));
+        Member author = rq.getActor();
 
         Post post = postService.create(author, reqBody.title(), reqBody.content());
 
