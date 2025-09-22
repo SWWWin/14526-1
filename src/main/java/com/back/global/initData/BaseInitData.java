6fbe1f382d4acd90ec37bb613668077060e49c19
SWWWin <bgpd1258@naver.com>
2025-09-22 11:26:30 +0900

    feat: static 의존성 주입

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -4,6 +4,7 @@ import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.service.MemberService;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import com.back.global.app.AppConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -37,18 +38,18 @@ public class BaseInitData {
         if(memberSerivce.count() > 0) return;
 
         Member memberSystem = memberSerivce.join("system", "1234", "시스템");
-        memberSystem.modifyApiKey(memberSystem.getUsername());
+        if(AppConfig.isNotProd()) memberSystem.modifyApiKey(memberSystem.getUsername());
 
         Member memberAdmin = memberSerivce.join("admin", "1234", "관리자");
-        memberAdmin.modifyApiKey(memberAdmin.getUsername());
+        if(AppConfig.isNotProd()) memberAdmin.modifyApiKey(memberAdmin.getUsername());
         Member memberUser1 = memberSerivce.join("user1", "1234", "유저1");
-        memberUser1.modifyApiKey(memberUser1.getUsername());
+        if(AppConfig.isNotProd()) memberUser1.modifyApiKey(memberUser1.getUsername());
 
         Member memberUser2 = memberSerivce.join("user2", "1234", "유저2");
-        memberUser2.modifyApiKey(memberUser2.getUsername());
+        if(AppConfig.isNotProd()) memberUser2.modifyApiKey(memberUser2.getUsername());
 
         Member memberUser3 = memberSerivce.join("user3", "1234", "유저3");
-        memberUser3.modifyApiKey(memberUser3.getUsername());
+        if(AppConfig.isNotProd()) memberUser3.modifyApiKey(memberUser3.getUsername());
     }
 
 
