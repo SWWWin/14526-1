d145bfba59615772e785a78e3951834eb5be38b0
SWWWin <bgpd1258@naver.com>
2025-09-22 15:34:39 +0900

    feat: jwt 테스트

---
+++ src/test/java/com/back/domain/member/member/service/AuthTokenServiceTest.java
@@ -0,0 +1,66 @@
+package com.back.domain.member.member.service;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@ActiveProfiles("test") // 테스트 환경에서는 test 프로파일을 활성화합니다.
+@SpringBootTest // 스프링부트 테스트 클래스임을 나타냅니다.
+@Transactional // 각 테스트 메서드가 종료되면 롤백됩니다.
+public class AuthTokenServiceTest {
+    @Autowired
+    private AuthTokenService authTokenService;
+
+    @Test
+    @DisplayName("authTokenService가 존재한다.")
+    void t1() {
+        assertThat(authTokenService).isNotNull();
+    }
+
+    @Test
+    @DisplayName("jjwt 최신 방식으로 jwt 생성, {name=\"Paul\", age = 23}")
+    void t2() {
+        //서명 키
+        //서명 알고리즘
+        //클레임 토큰에 담을 데이터
+        //생성시간
+        //만료시간
+        long expireMillis = 1000L * 60 * 60 * 24 * 365; // 토큰 만료 시간 1년
+        Date issuedAt = new Date();
+        Date expiration = new Date(issuedAt.getTime() + expireMillis); //발행 시간으로부터 만료시간 설정
+
+        String originSecretKey = "your-very-long-secret-key-of-at-least-32-chars";
+        byte[] keyBytes = originSecretKey.getBytes(StandardCharsets.UTF_8);
+
+        SecretKey secretKey = Keys.hmacShaKeyFor(keyBytes);
+
+        //클레임 - 사용자 정보
+        Map<String, Object> claims = Map.of("name", "Paul", "age", "23");
+
+        String jwt = Jwts.builder()
+                .claims(claims) //사용자 정보
+                .issuedAt(issuedAt) //생성 날짜
+                .expiration(expiration) // 만료 날짜
+                .signWith(secretKey) // 키 서명
+                .compact();
+
+        assertThat(jwt).isNotBlank();
+
+        System.out.println("jwt: " + jwt);
+    }
+}
